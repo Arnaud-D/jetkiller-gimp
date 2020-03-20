14cbb47425d039f2696818437510f8e2b2a07fbc
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2020-03-20 17:32:54 +0100

    Improve performance massively.

--- jetkiller.py
+++ jetkiller.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
+import gimp
+import gimpenums
+import gimpplugin
+
 import pygtk
 import gtk
 import numpy as np
 from matplotlib import cm
-import gimpfu as gf
-
-pygtk.require("2.0")
 
 # Internal package parameters
 _colormap_size = 512
 _nint = 255
-_type = float
+_type = int
 _cache_size = 1024
+_tile_cache_size = 16
+
+pygtk.require("2.0")
 
 
 def get_colormap(colormap):
@@ -26,67 +30,77 @@ def get_colormap(colormap):
 
 
 def jetkiller(img, colormap):
-    gf.pdb.gimp_image_undo_group_start(img)  # start undo group
-    gf.gimp.progress_init("Executing Jet Killer (0%)")
+    gimp.pdb.gimp_image_undo_group_start(img)  # start undo group
+    gimp.progress_init("Executing Jet Killer ...")
 
-    active_layer = gf.pdb.gimp_image_get_active_layer(img)
-    new_layer = gf.pdb.gimp_layer_new(img,
-                                      active_layer.width,
-                                      active_layer.height,
-                                      gf.RGBA_IMAGE,
-                                      active_layer.name + " - Jet Killer",
-                                      100.,
-                                      gf.NORMAL_MODE)
-    gf.pdb.gimp_image_insert_layer(img, new_layer, None, 0)
+    active_layer = gimp.pdb.gimp_image_get_active_layer(img)
+    new_layer = gimp.pdb.gimp_layer_new(img,
+                                        active_layer.width,
+                                        active_layer.height,
+                                        gimpenums.RGBA_IMAGE,
+                                        active_layer.name + " - Jet Killer",
+                                        100.,
+                                        gimpenums.NORMAL_MODE)
+    gimp.pdb.gimp_image_insert_layer(img, new_layer, None, 0)
 
-    (selection_exists, start_x, start_y, end_x, end_y) = gf.pdb.gimp_selection_bounds(img)
+    (selection_exists, start_x, start_y, end_x, end_y) = gimp.pdb.gimp_selection_bounds(img)
     if not selection_exists:
-        gf.pdb.gimp_selection_all(img)
-        (_, start_x, start_y, end_x, end_y) = gf.pdb.gimp_selection_bounds(img)
+        gimp.pdb.gimp_selection_all(img)
+        (_, start_x, start_y, end_x, end_y) = gimp.pdb.gimp_selection_bounds(img)
 
     input_cmap = get_colormap("jet")
     output_cmap = get_colormap(colormap)
 
-    def convert_pixel(red, green, blue):
+    def raw_convert_pixel(px):
         # Get nearest color from input colormap and return
         # corresponding color in output colormap
-        dr = input_cmap[:, 0] - red
-        dg = input_cmap[:, 1] - green
-        db = input_cmap[:, 2] - blue
+        dr = input_cmap[:, 0] - ord(px[0])
+        dg = input_cmap[:, 1] - ord(px[1])
+        db = input_cmap[:, 2] - ord(px[2])
         dist = dr * dr + dg * dg + db * db
         idx = np.argmin(dist)
-        return output_cmap[idx]
+        return "".join([chr(k) for k in output_cmap[idx]])
 
-    for y in range(start_y, end_y + 1):
-        for x in range(start_x, end_x + 1):
-            if gf.pdb.gimp_selection_value(img, x, y) > 0:  # not selected pixels are ignored
-                (nchannels, pixel_value) = gf.pdb.gimp_drawable_get_pixel(active_layer, x, y)
-                r, g, b = pixel_value[0:3]
-                if r != g or g != b:  # Grey pixels are not processed
-                    (new_r, new_g, new_b) = convert_pixel(r, g, b)
-                    if nchannels == 4:
-                        new_pixel_value = (new_r, new_g, new_b, pixel_value[3])
-                    else:
-                        new_pixel_value = (new_r, new_g, new_b, 255)
-                    gf.pdb.gimp_drawable_set_pixel(new_layer, x, y, 4, new_pixel_value)
+    cache = {}
 
-        new_layer.update(0, 0, active_layer.width, active_layer.height)
+    def convert_pixel(px):
+        if px not in cache:
+            cache[px] = raw_convert_pixel(px)
+        return cache[px]
 
-        progress_ratio = float(y - start_y) / (end_y - start_y + 1)
-        gf.gimp.progress_update(progress_ratio)  # update progress bar
-        gf.pdb.gimp_progress_set_text("Executing Jet Killer ({:.1f}%)".format(progress_ratio * 100))
-        gf.pdb.gimp_displays_flush()
+    gimp.tile_cache_ntiles(_tile_cache_size)
+    src_rgn = active_layer.get_pixel_rgn(start_x, start_y, end_x - start_x, end_y - start_y, False, False)
+    dst_rgn = new_layer.get_pixel_rgn(start_x, start_y, end_x - start_x, end_y - start_y, True, True)
+
+    for x in xrange(start_x, end_x):
+        for y in xrange(start_y, end_y):
+            pixel = src_rgn[x, y]
+            if pixel[0] != pixel[1] or pixel[1] != pixel[2]:  # Grey pixels are not processed
+                new_pixel = convert_pixel(pixel[0:3])
+                if len(pixel) == 4:  # number of channels
+                    new_pixel_value = new_pixel + pixel[3]
+                else:
+                    new_pixel_value = new_pixel + chr(255)
+                dst_rgn[x, y] = new_pixel_value
+
+        progress_ratio = float(x - start_x) / (end_x - start_x)
+        gimp.progress_update(progress_ratio)  # update progress bar
+
+    new_layer.flush()
+    new_layer.merge_shadow()
+    new_layer.update(start_x, start_y, end_x - start_x, end_y - start_y)
 
     if not selection_exists:  # remove selection when added by the script
-        gf.pdb.gimp_selection_none(img)
+        gimp.pdb.gimp_selection_none(img)
 
-    gf.pdb.gimp_progress_end()
-    gf.pdb.gimp_displays_flush()
-    gf.pdb.gimp_image_undo_group_end(img)  # end undo group
+    gimp.pdb.gimp_progress_end()
+    gimp.pdb.gimp_displays_flush()
+    gimp.pdb.gimp_image_undo_group_end(img)  # end undo group
 
 
 class Dialog:
     def __init__(self, img):
+
         # Dialog configuration
         self.dialog = gtk.Dialog()
         self.dialog.set_title("Jet Killer")
@@ -106,6 +120,7 @@ class Dialog:
         self.cm_combobox.pack_start(cell)
         self.cm_combobox.add_attribute(cell, 'text', 0)
         colormaps = ["viridis", "plasma", "inferno", "magma", "cividis"]
+        self.colormap = colormaps[0]  # default colormap
         for c in colormaps:
             self.store.append([c])
         self.cm_combobox.set_model(self.store)
@@ -127,52 +142,59 @@ class Dialog:
         # Events
         self.dialog.connect("destroy", gtk.main_quit)
         button_ok.connect("clicked", self.on_click_ok)
-        button_close.connect_object("clicked", gtk.Widget.destroy, self)
+        button_close.connect_object("clicked", gtk.Widget.destroy, self.dialog)
 
-        self.img = img
-        gtk.main_iteration()
+        gtk.main()
 
     def on_click_ok(self, arg):
+        self.colormap = self.cm_combobox.get_active_text()
+        self.dialog.destroy()
         gtk.main_quit()
-        colormap = self.cm_combobox.get_active_text()
-        jetkiller(self.img, colormap)
 
 
-class JetkillerPlugin:
+class Jetkiller(gimpplugin.plugin):
     def __init__(self):
-        self.proc_name = "python_fu_jetkiller"
+        self.name = "jetkiller"
         self.blurb = "Improve an image using the 'jet' colormap"
         self.help = "Improve an image using the 'jet' colormap by converting it to a better colormap"
         self.author = "Arnaud-D"
         self.copyright = self.author
         self.date = "2020"
-        self.label = "Jet Killer"
-        self.imagetypes = "RGB*"
-        self.params = [(gf.PF_IMAGE, "image", "Input image", None)]
-        self.results = []
-        self.menu = "<Image>/Filters/Enhance"
+        self.menupath = "<Image>/Filters/Enhance/Jet Killer..."
+        self.image_types = "RGB*"
+        self.type = gimpenums.PLUGIN
+        self.params = [(gimpenums.PDB_INT32, "run-mode", "Run mode"),
+                       (gimpenums.PDB_IMAGE, "image", "Input image")]
+        self.ret_vals = []
 
-    def register(self):
-        gf.register(
-            self.proc_name,
+    def query(self):
+        gimp.install_procedure(
+            self.name,
             self.blurb,
             self.help,
             self.author,
             self.copyright,
             self.date,
-            self.label,
-            self.imagetypes,
+            self.menupath,
+            self.image_types,
+            self.type,
             self.params,
-            self.results,
-            self.function,
-            menu=self.menu
-        )
+            self.ret_vals)
 
-    def function(self, img):  # number of arguments (excluding self) should be len(self.params)
-        dialog = Dialog(img)
-        gtk.main()
+    def jetkiller(self, run_mode, image):
+        if run_mode == gimpenums.RUN_INTERACTIVE:
+            dialog = Dialog(image)
+            colormap = dialog.colormap
+        elif run_mode == gimpenums.RUN_NONINTERACTIVE:
+            raise NotImplementedError("Non-interactive mode is not implemented (yet).")
+        elif run_mode == gimpenums.RUN_WITH_LAST_VALS:
+            raise NotImplementedError("Repeat with last values is not implemented (yet).")
+        else:
+            raise ValueError("Invalid run mode. "
+                             "Should be RUN_INTERACTIVE (0), RUN_NONINTERACTIVE (1) or RUN_WITH_LAST_VALS (2).")
+
+        jetkiller(image, colormap)
 
 
-jetkiller_plugin = JetkillerPlugin()
-jetkiller_plugin.register()
-gf.main()
+if __name__ == '__main__':
+    Jetkiller().start()
